711c2418e04bf7c0bbb3d8cb8c12103f2b94189f
TalyssonG <lopestalysson99@gmail.com>
2021-06-15 15:30:33 -0300

    Projeto Finalizado

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -37,7 +37,7 @@ export default function Episode({ episode }: EpisodeProps) {
   return (
     <div className={styles.episode}>
       <Head>
-        <title>{episode.title} | Podcastr</title>
+        <title>{episode.title} | Podcastr  </title>
       </Head>
 
 
